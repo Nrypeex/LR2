e6825dd4a042f9ae2d92938f66b03dabeaed4549
Nrypeex <nrypeex@yandex.ru>
2022-10-21 15:26:59 +0300

    alg for 2

--- zadanie2.py
+++ zadanie2.py
@@ -1,4 +1,7 @@
 def zad2():
+    doFor2 = []
+    doFor3 = []
+    doFor5 = []
     intList = []
 
     print("input >0 nums")
@@ -9,5 +12,16 @@ def zad2():
         if inputInteger == 0:
             break
         intList.append(inputInteger)
+    for i in range(len(intList)) :
+        if(intList[i] % 2 == 0): doFor2.append(intList[i])
+        if(intList[i] % 3 == 0): doFor3.append(intList[i])
+        if(intList[i] % 5 == 0): doFor5.append(intList[i])
+    print(doFor2)
+    print(doFor3)
+    print(doFor4)
+
+
+            
+        
     
     print(intList)
