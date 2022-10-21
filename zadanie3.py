53c90698e41bc95fd4ff2ae52fbc030381f782fe
Nrypeex <nrypeex@yandex.ru>
2022-10-21 19:20:16 +0300

    3rd working

--- zadanie3.py
+++ zadanie3.py
@@ -1,12 +1,15 @@
 def zad3():
     
-    invrs = []
+    invrs = 0
     print("iput digit")
     a = int(input())
     b = a
+    c= 0
     while b > 0:
-        invrs.append(int(a%10))
+        invrs += int(a%10)
+        invrs *= 10
         a=a/10
         b = int(b/10)
-    print (a)
-    print(invrs)
+        c+=1
+    print (a*(10**c))
+    print(invrs/10)
