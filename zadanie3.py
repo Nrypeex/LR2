5069092f6f011f886bb56a54743f2d5fbb010824
Nrypeex <nrypeex@yandex.ru>
2022-10-21 19:27:25 +0300

    negative

--- zadanie3.py
+++ zadanie3.py
@@ -3,13 +3,14 @@ def zad3():
     invrs = 0
     print("iput digit")
     a = int(input())
-    b = a
+    b = abs(a)
     c= 0
     while b > 0:
-        invrs += int(a%10)
+        invrs += int(b%10)
         invrs *= 10
         a=a/10
         b = int(b/10)
         c+=1
     print (a*(10**c))
+    if a<0 : invrs *= -1
     print(invrs/10)
